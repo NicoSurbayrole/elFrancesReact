eb2db4ae8b9b9bec0da7ae57299a105166f4f111
nicolas <nico.surba@hotmail.com>
2025-11-09 15:31:38 -0300

    sacamos un import innecesario

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,5 +1,3 @@
-import { NavLink } from "react-router-dom";
-
 import style from "./navbar.module.css";
 import NavLinks from "../navLinks/NavLinks";
 import NavIconContainer from "../nav_icon_container/NavIconContainer";
