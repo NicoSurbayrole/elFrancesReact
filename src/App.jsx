8846f6f08d1da46d94b9547c3fc8ed5ddb4c7871
nicolas <nico.surba@hotmail.com>
2025-11-09 15:58:09 -0300

    agregamos la importacion para las rutas y la ruta main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./pages/navBar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
 
@@ -6,6 +8,9 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
