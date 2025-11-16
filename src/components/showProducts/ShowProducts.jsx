2ce2441bce5868517cc4255bc20f15f02d20ba10
nicolas <nico.surba@hotmail.com>
2025-11-15 23:34:07 -0300

    componente para los productos y sus estilos

---
+++ src/components/showProducts/ShowProducts.jsx
@@ -0,0 +1,34 @@
+import style from "./showProducts.module.css";
+
+const ShowProducts = ({ items }) => {
+  return (
+    <>
+      <header>
+        <h2 className={style.titel_articulos}>Todos los Artículos</h2>
+      </header>
+      <div className={style.products_contain}>
+        <aside className={style.filtros}></aside>
+        {items.map((item) => (
+          <div key={item.product_id}>
+            <section accessKey={item.product_id} className={style.products}>
+              <header>
+                <img
+                  src={item.imagUrl}
+                  className={style.imagen_producto}
+                  alt="producto"
+                />
+              </header>
+              <div className={style.info_products}>
+                <h3>{item.name}</h3>
+                <small>{item.price}$usd</small>
+                <button accessKey={item.product_id}>Ver producto</button>
+              </div>
+            </section>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ShowProducts;
