63793361aa3fb7d9442786e505faaa33bf7aa256
nicolas <nico.surba@hotmail.com>
2025-11-15 23:32:32 -0300

    agregamos las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/navBar/Navbar.jsx";
+import Products from "./components/products/Products.jsx";
+import NotFound from "./pages/notFound/NotFound.jsx";
 
 function App() {
 
@@ -6,6 +9,10 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Products/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
     </>
   )
 }
