83e8910014418957e7eddaeedb1fb43a8f05dbe5
nicolas <nico.surba@hotmail.com>
2025-11-16 15:08:04 -0300

    agregamos la ruta del componente sobre nosotrso

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/navBar/Navbar.jsx";
 import Products from "./components/products/Products.jsx";
 import NotFound from "./pages/notFound/NotFound.jsx";
+import AboutUs from "./components/aboutUs/AboutUs.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Products/>} />
+        <Route path="/sobre-nosotros" element={<AboutUs/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </>
