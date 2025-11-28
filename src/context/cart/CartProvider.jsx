1c9da2b838d0b1fcec4a617cc58de6ec9fef0974
nicolas <nico.surba@hotmail.com>
2025-11-28 14:44:34 -0300

    creamos el cart provider con sus funcionalidades

---
+++ src/context/cart/CartProvider.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { CartContext } from "./CartContext";
+
+const CartProvider = ({ children }) => {
+  const [cartCount, setCartCount] = useState([]);
+
+  const getTotalProductos = () =>
+    cartCount.reduce((acc, current) => acc + current.count, 0);
+
+  const addToCart = (producto) =>
+    setCartCount((cart) => {
+      const productoRepetido = cart.find((item) => item.product_id === producto.product_id);
+
+      if (!productoRepetido) {
+        producto.count = 1;
+        return [...cart, producto];
+      }
+
+      if (productoRepetido.count <= productoRepetido.stock) {
+        producto.count = 1;
+        producto.count++;
+        return [...cart];
+      }
+      return cart;
+    });
+
+    const getProductFiltrado = (id) =>{
+      const productoCarrito = cartCount.find((procuto) => Number(id) === procuto.product_id) 
+      return productoCarrito
+    }
+
+  return (
+    <CartContext.Provider value={{ getTotalProductos, addToCart, getProductFiltrado }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
